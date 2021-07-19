b389c0080a10154377a987a78d9f3e754b37837c
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-07-19 23:23:40 +0100

    app is fully functional, styling form here on

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,24 +1,30 @@
 class BookmarksController < ApplicationController
+  before_action :find_list
+  # before_action :find_movie, only: [:create]
 
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
   
-
   def create
-
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render "new"
+    end
   end
   
 
 private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
   
-
-
-
+  def find_list
+    @list = List.find(params[:list_id])
+  end
 
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,26 +3,23 @@ class ListsController < ApplicationController
 
   def index
    @lists = List.all 
-   @list = List.new    
-
   end
   
   def show
     @list = List.find(params[:id])
   end
   
-  # def new
-    # @list = List.new    
-  # end
+  def new
+    @list = List.new    
+  end
   
   def create
     @list = List.new(list_params)
-    if @list.save 
-    redirect_to lists_path
-    else 
-      render "lists/new"
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
     end
-  
   end
   
 private

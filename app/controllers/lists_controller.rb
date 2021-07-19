6596f8ae8fe70c25ff6c78119c5f038bdf5afa99
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-07-19 14:47:02 +0100

    moved the new list page to the index view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,16 +2,18 @@ class ListsController < ApplicationController
 
 
   def index
-   @lists = List.all  
+   @lists = List.all 
+   @list = List.new    
+
   end
   
   def show
     @list = List.find(params[:id])
   end
   
-  def new
-    @list = List.new    
-  end
+  # def new
+    # @list = List.new    
+  # end
   
   def create
     @list = List.new(list_params)
